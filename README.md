011eb4e07908e823af4e8d7c44605798e4ecd569
Chris Russell <chris.russell02@gmail.com>
2022-11-20 09:00:18 -0500

    seo

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ npm run dev
 ## 2022/2023 Planning
 
 To do:
+- [X] Improve SEO
 - [ ] Add images to blog posts
 - [ ] Add socials to footer
 - [ ] Do actual full stack projects
--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "chris russell jr •";
-export const SITE_DESCRIPTION = "supply chain @ spacex";
+export const SITE_DESCRIPTION = "ESTP. Aspiring engineer. Into big problems. Always learning. Supply Chain Technology @ SpaceX 👨‍💻🚀 ";
