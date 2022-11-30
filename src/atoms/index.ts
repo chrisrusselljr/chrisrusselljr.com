134de76b4016bd9a2a0ac35637c6cadeda90c811
Chris Russell <chris.russell02@gmail.com>
2022-11-30 00:03:33 -0500

    fix

--- src/atoms/index.ts
+++ src/atoms/index.ts
@@ -1,15 +1,4 @@
-// import { atom } from "nanostores";
-
 import { map } from "nanostores";
-
-// export interface ProfileValue {
-//   name: string,
-//   email?: string
-// }
-
-// export const profile = map<ProfileValue>({
-//   name: 'anonymous'
-// })
 
 import type { User } from "../types/index";
 
@@ -18,11 +7,3 @@ export const userData = map<User>(null);
 export const setUser = (data: User | null) => {
   userData.set(data);
 };
-
-// import type { User } from "../types/index";
-
-// export const user = atom<null | User>(null);
-
-// export const setUser = (data: User | null) => {
-//   user.set(data);
-// };
