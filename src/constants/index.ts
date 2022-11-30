b0d9e89664fcbdf712b410f0fab64622d707bf49
Chris Russell <chris.russell02@gmail.com>
2022-11-29 23:37:25 -0500

    secret

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
 export const AUTH_COOKIE_NAME = "__session_crj";
 export const JWT_SECRET = import.meta.env.JWT_SECRET;
-export const SIGN_IN_WITH_GOOGLE_CLIENT_ID = import.meta.env.SIGN_IN_WITH_GOOGLE_CLIENT_ID;
+export const PUBLIC_SIGN_IN_WITH_GOOGLE_CLIENT_ID = import.meta.env.PUBLIC_SIGN_IN_WITH_GOOGLE_CLIENT_ID;
