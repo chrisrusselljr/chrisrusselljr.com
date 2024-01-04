d89451a4d095f272dfa2973df561265ae8fa6440
Chris Russell <chris.russell02@gmail.com>
2024-01-04 14:10:53 -0800

    tesst

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   adapter: netlify({
     edgeMiddleware: true
   }),
