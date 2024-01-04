6370f1a71597799ff258d359ce1ed12526a3f250
Chris Russell <chris.russell02@gmail.com>
2024-01-04 09:37:22 -0800

    fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,55 +6,47 @@ import react from "@astrojs/react";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { remarkReadingTime } from "./src/utils/reading-time.mjs";
 
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 
 // https://astro.build/config
-import image from "@astrojs/image";
 
 // https://astro.build/config
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify";
+
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: node({
+    mode: 'standalone'
+  }),
   site: "https://www.chrisrusselljr.com",
   markdown: {
     extendDefaultPlugins: true,
     rehypePlugins: [rehypeAutolinkHeadings],
     remarkPlugins: [remarkReadingTime],
     shikiConfig: {
-      theme: "vitesse-dark",
-    },
+      theme: "vitesse-dark"
+    }
   },
-  integrations: [
-    mdx(),
-    sitemap({
-      customPages: [
-        "https://www.chrisrusselljr.com/blog",
-        // "https://www.chrisrusselljr.com/guestbook",
-        "https://www.chrisrusselljr.com/blog/empowerment",
-        "https://www.chrisrusselljr.com/blog/five-tips-for-effective-team-members",
-        "https://www.chrisrusselljr.com/blog/dont-ask-for-feedback",
-      ],
-    }),
-    tailwind({
-      // Example: Disable injecting a basic `base.css` import on every page.
-      // Useful if you need to define and/or import your own custom `base.css`.
-      applyBaseStyles: false,
-    }),
-    react(),
-    prefetch(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-  ],
+  integrations: [mdx(), sitemap({
+    customPages: ["https://www.chrisrusselljr.com/blog",
+    // "https://www.chrisrusselljr.com/guestbook",
+    "https://www.chrisrusselljr.com/blog/empowerment", "https://www.chrisrusselljr.com/blog/five-tips-for-effective-team-members", "https://www.chrisrusselljr.com/blog/dont-ask-for-feedback"]
+  }), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false
+  }), react(), prefetch()],
   build: {
     inlineStylesheets: 'never'
   },
-  adapter: netlify(),
+
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './components/**/*.{js,ts,jsx,tsx}', './app/**/*.{js,ts,jsx,tsx}',],
   theme: {
     extend: {
       colors: {
