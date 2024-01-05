f22cf4c582b0339d9c83f4595f35846693c98787
Chris Russell <chris.russell02@gmail.com>
2024-01-04 18:07:08 -0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: "server",
   adapter: netlify({
     edgeMiddleware: true
   }),
